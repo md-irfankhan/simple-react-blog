dfd1f78019e4f1de591793aaac17cb4eab18bb70
Md. Irfan Khan <learn.irfankhan@gmail.com>
2025-07-20 07:25:38 +0600

    Card added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
 
+import { useEffect, useState } from 'react'
 import './App.css'
 import Nav from './components/Nav/Nav'
+import Cards from './components/Cards/Cards';
 
 function App() {
+  const [blogs,setBlogs]=useState([]);
+  useEffect(()=>{
+    fetch('api/api.json').then(res=>res.json()).
+    then(data=>setBlogs(data.posts))
+  },[])
+  console.log(blogs)
   
-
   return (
     <>
      <Nav></Nav>
+     <div className='flex max-w-[1280px] mx-auto'>
+      <div className='w-2/3 flex flex-col gap-6 mt-8'>
+           {
+            blogs.map(blog=><Cards key={blog.id} blog={blog}></Cards>)
+           }
+      </div>
+      <div className='w-1/3'>
+        
+      </div>
+     </div>
     </>
   )
 }
---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,41 @@
+
+import { CiBookmarkPlus } from "react-icons/ci";
+const Cards = ({blog}) => {
+    const {coverImg,authorPhoto,authorName,postedDate,readingTime,blogTitle,hashtags}=blog
+    return (
+        <div>
+            <div>
+                <img className="h-[450px] w-full object-cover rounded-2xl" src={coverImg} alt="" />
+            </div>
+            <div className="mt-[34px]">
+                <div className="flex justify-between">
+                    <div className="flex items-center gap-2">
+                        <div>
+                            <img className="h-[50px] w-[50px] rounded-full" src={authorPhoto} alt="" />
+                        </div>
+                        <div>
+                            <h2 className="text-[24px] font-bold exo-2 mb-0">{authorName}</h2>
+                            <p className="exo-2 font-semibold opacity-[0.5] mt-0">{postedDate}</p>
+                        </div>
+                    </div>
+                    <div className="flex items-center gap-2">
+                        <p className="exo-2 font-semibold opacity-[0.5] mt-0">{readingTime}</p>
+                        <div><CiBookmarkPlus className="h-[60px]" /></div>
+
+                    </div>
+                </div>
+
+                <div>
+                    <h2 className="exo-2 font-bold text-[40px]">{blogTitle}</h2>
+                    <p className="font-medium opacity-[0.5]">{hashtags.map(hash=><span>{hash} </span>)}</p>
+                    <button className="text-blue-400 underline font-semibold hover:text-cyan-400">Mark as read</button>
+                </div>
+
+            </div>
+            <hr className="opacity-[0.1] mt-5" />
+
+        </div>
+    );
+};
+
+export default Cards;
